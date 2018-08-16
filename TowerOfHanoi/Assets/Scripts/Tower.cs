76a286a5147a3165e03f26fc2b965ad221319966
Krasnogorov Sergey <shamanru@meta.ua>
2018-08-17 00:11:58 +0300

    Added scripts for visualiation of moving disks

---
+++ TowerOfHanoi/Assets/Scripts/Tower.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Class described tower
+ */
+public class Tower : MonoBehaviour
+{
+    // List of disks on this tower
+    private List<Disk> mDiskList = new List<Disk>();
+    // Empty game object on the scene which helps to disks in moves
+    public GameObject movingHelper;
+    // Prefab of disk
+    public GameObject diskPrefab;
+    /**
+     * Initialize tower with count of disks.
+     * Instantiate disk object and setup correct information
+     */
+    public void InitWithCount(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject diskObj = GameObject.Instantiate<GameObject>(diskPrefab);
+            Disk disk = diskObj.GetComponent<Disk>();
+            disk.InitWithSize(count - i);
+            SetDiskToTop(diskObj);
+            mDiskList.Add(disk);
+        }
+    }
+    /**
+     * Setup disk position at top position of tower
+     */
+    private void SetDiskToTop(GameObject diskObj)
+    {
+        diskObj.transform.position = GetTopPosition();
+    }
+    /**
+     * Start moving procedure for top disk
+     */
+    public void MoveRingTo(Tower newTower)
+    {
+        mDiskList[mDiskList.Count - 1].MoveFromTo(this, newTower);
+        mDiskList.RemoveAt(mDiskList.Count - 1);
+    }
+    /**
+     * Update list of disks in tower
+     */
+    public void FinishMovingDisk(Disk newDisk)
+    {
+        mDiskList.Add(newDisk);
+        // TODO: Notify simulator - step finished
+    }
+    /**
+     * Get position for top disk
+     */
+    public Vector3 GetTopPosition()
+    {
+        return new Vector3(transform.position.x, -3 + mDiskList.Count, transform.position.z);
+    }
+}
